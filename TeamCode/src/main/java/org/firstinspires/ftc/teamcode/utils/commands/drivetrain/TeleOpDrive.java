84761b3d0d0f7e0bebe6fd74983b93e997b4e1a3
Rohan Gopalan <130528515+ElectroRG@users.noreply.github.com>
2025-08-17 12:27:25 -0500

    add outtake slides and drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/commands/drivetrain/TeleOpDrive.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.utils.commands.drivetrain;
+
+public class TeleOpDrive {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/subsystems/Drivetrain.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.utils.subsystems;
+
+public class Drivetrain {
+}
