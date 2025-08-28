875d1b25acca5bf32bd4583ef3262b8cab366478
Rohan Gopalan <130528515+ElectroRG@users.noreply.github.com>
2025-08-28 16:10:09 -0500

    inverse kin and friends

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SoloTeleOp.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="TeleOp")
+public class SoloTeleOp extends OpMode {
+    /**
+     * User-defined init method
+     * <p>
+     * This method will be called once, when the INIT button is pressed.
+     */
+    @Override
+    public void init() {
+
+    }
+
+    /**
+     * User-defined loop method
+     * <p>
+     * This method will be called repeatedly during the period between when
+     * the play button is pressed and when the OpMode is stopped.
+     */
+    @Override
+    public void loop() {
+
+    }
+}
