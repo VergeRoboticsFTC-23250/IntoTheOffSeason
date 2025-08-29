8a8d49cba8fb2578e9158375c6d28694d347947b
Rohan Gopalan <130528515+ElectroRG@users.noreply.github.com>
2025-08-28 22:55:11 -0500

    augh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/SplineTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/SplineTesting.java
@@ -6,7 +6,7 @@ import com.pedropathing.pathgen.PathBuilder;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
 
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.utils.Globals;
 import org.firstinspires.ftc.teamcode.utils.Robot;
 
 @Autonomous
-public class SplineTesting extends OpMode {
+public class SplineTesting extends CommandOpMode {
 
     public Robot robot;
     public static PathBuilder builder = new PathBuilder();
@@ -22,27 +22,22 @@ public class SplineTesting extends OpMode {
     public static PathChain line1 = builder
             .addPath(
                     new BezierLine(
-                            new Point(7.940, 37.895, Point.CARTESIAN),
-                            new Point(39.158, 69.835, Point.CARTESIAN)
+                            new Point(0, 0, Point.CARTESIAN),
+                            new Point(10, 0, Point.CARTESIAN)
                     )
             )
             .setConstantHeadingInterpolation(Math.toRadians(0))
             .build();
 
     @Override
-    public void init() {
+    public void initialize() {
         Globals.init(Globals.MatchState.AUTONOMOUS, this);
-        robot = new Robot(this, new Pose(8, 38, 0));
-        robot.drivetrain.follower.setPose(new Pose(8, 38, 0));
+        robot = new Robot(this, new Pose(0, 0, 0));
+        robot.drivetrain.follower.setPose(new Pose(0, 0, 0));
 
 
         CommandScheduler.getInstance().registerSubsystem(robot.drivetrain);
 
-
-    }
-
-    @Override
-    public void start() {
         CommandScheduler.getInstance().schedule(
                 new FollowPathCommand(
                         robot.drivetrain.follower,
@@ -51,14 +46,18 @@ public class SplineTesting extends OpMode {
         );
     }
 
+    @Override
+    public void run() {
+        CommandScheduler.getInstance().run();
+        robot.update();
+    }
+
+
     /**
      * User-defined loop method
      * <p>
      * This method will be called repeatedly during the period between when
      * the play button is pressed and when the OpMode is stopped.
      */
-    @Override
-    public void loop() {
-        robot.update();
-    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -34,11 +34,11 @@ public class FConstants {
         FollowerConstants.useSecondaryHeadingPID = false;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.015,0,0.00015,0.6,0);
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.015,0,0.00009,0.6,0);
         FollowerConstants.useSecondaryDrivePID = false;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
-        FollowerConstants.zeroPowerAccelerationMultiplier = 2.5;
+        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
         FollowerConstants.centripetalScaling = 0.0005;
 
         FollowerConstants.pathEndTimeoutConstraint = 50;
