5f37fcf85930c72e318365165dbd37a42f1cd13b
vivekmaddineni <vivekmaddineni@gmail.com>
2025-08-29 00:18:28 -0500

    Fixed follower bullshit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/SplineTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/SplineTesting.java
@@ -1,63 +1,51 @@
 package org.firstinspires.ftc.teamcode.opmodes.testing;
 
+import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
 import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathBuilder;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 import org.firstinspires.ftc.teamcode.utils.Globals;
 import org.firstinspires.ftc.teamcode.utils.Robot;
 
 @Autonomous
 public class SplineTesting extends CommandOpMode {
-
+    public static double DISTANCE = 40;
     public Robot robot;
-    public static PathBuilder builder = new PathBuilder();
-
-    public static PathChain line1 = builder
-            .addPath(
-                    new BezierLine(
-                            new Point(0, 0, Point.CARTESIAN),
-                            new Point(10, 0, Point.CARTESIAN)
-                    )
-            )
-            .setConstantHeadingInterpolation(Math.toRadians(0))
-            .build();
+    //private Follower follower;
+    private Path path;
 
     @Override
     public void initialize() {
+        //initialize ftclib & subsystems
         Globals.init(Globals.MatchState.AUTONOMOUS, this);
         robot = new Robot(this, new Pose(0, 0, 0));
-        robot.drivetrain.follower.setPose(new Pose(0, 0, 0));
-
-
         CommandScheduler.getInstance().registerSubsystem(robot.drivetrain);
 
+        //initialize path
+        path = new Path(new BezierLine(new Point(0,0, Point.CARTESIAN), new Point(DISTANCE,0, Point.CARTESIAN)));
+        path.setConstantHeadingInterpolation(0);
+
+        //schedule command
         CommandScheduler.getInstance().schedule(
                 new FollowPathCommand(
                         robot.drivetrain.follower,
-                        line1, true
+                        path, true
                 )
         );
     }
-
     @Override
     public void run() {
-        CommandScheduler.getInstance().run();
-        robot.update();
+        robot.drivetrain.follower.update();
     }
-
-
-    /**
-     * User-defined loop method
-     * <p>
-     * This method will be called repeatedly during the period between when
-     * the play button is pressed and when the OpMode is stopped.
-     */
-
 }
